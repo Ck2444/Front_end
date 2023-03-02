87f1f1ccc28136a16e34da02e19ab3b27cbcf1a3
konstantinevstigneev <t777tt88@gmai.com>
2023-03-02 16:29:39 +0100

    HW9

--- HW9_28_02_2023/scrypt.js
+++ HW9_28_02_2023/scrypt.js
@@ -8,13 +8,14 @@ divSquare.style.backgroundColor = "#ff0000";
 document.body.append(divSquare);
 
 const button = document.createElement("button");
-button.textContent = " Buttron";
+button.textContent = " Button";
 button.style.padding = '5px 10px';
 button.style.backgroundColor = 'rgb(38, 38, 38)';
 button.style.color = '#fff';
 button.style.border = 'none';
 button.style.borderRadius = '5px';
 button.style.cursor = 'pointer';
+button.style.marginBottom = '20px';
 document.body.append(button);
 
 const square = document.querySelector("#mySquare");
@@ -27,77 +28,79 @@ button.addEventListener("click", function () {
 
 //The second task: Создать кнопку и розовый квадрат с размерами 200х200px. При клике на кнопку менять цвет на синий и выводить в консоль обновленный цвет квадрата.
 
-const divSquare = document.createElement("div");
-divSquare.id = "mySquare";
-divSquare.style.width = "200px";
-divSquare.style.height = "200px";
-divSquare.style.backgroundColor = "#ff9e9e";
-document.body.append(divSquare);
+const divPinkSquare = document.createElement("div");
+divPinkSquare.id = "myPinkSquare";
+divPinkSquare.style.width = "200px";
+divPinkSquare.style.height = "200px";
+divPinkSquare.style.backgroundColor = "#ff9e9e";
+document.body.append(divPinkSquare);
 
-const button = document.createElement("button");
-button.textContent = "Кнопка";
-button.style.padding = '5px 10px';
-button.style.backgroundColor = 'rgb(38, 38, 38)';
-button.style.color = '#fff';
-button.style.border = 'none';
-button.style.borderRadius = '5px';
-button.style.cursor = 'pointer';
-document.body.append(button);
+const button2 = document.createElement("button");
+button2.textContent = "Button2";
+button2.style.padding = '5px 10px';
+button2.style.backgroundColor = 'rgb(38, 38, 38)';
+button2.style.color = '#fff';
+button2.style.border = 'none';
+button2.style.borderRadius = '5px';
+button2.style.cursor = 'pointer';
+button2.style.marginBottom = '20px';
 
-const square = document.querySelector("#mySquare");
+document.body.append(button2);
 
-button.addEventListener("click", function () {
-    square.style.backgroundColor = "#0000ff";
-    console.log(`New squarecolour is ${square.style.backgroundColor}`);
+const pinkSquare = document.querySelector("#myPinkSquare");
+
+button2.addEventListener("click", function () {
+    pinkSquare.style.backgroundColor = "#0000ff";
+    console.log(`New squarecolour is ${pinkSquare.style.backgroundColor}`);
 });
 
 //  The third task: Создать кнопку и квадрат с размерами 150х150px. При клике на кнопку увеличивать высоту и ширину квадрата на 20px.
 
-const divSquare = document.createElement("div");
-divSquare.id = "mySquare";
-divSquare.style.width = "150px";
-divSquare.style.height = "150px";
-divSquare.style.backgroundColor = "#002e11";
-document.body.append(divSquare);
+const divSquare2 = document.createElement("div");
+divSquare2.id = "mySquare2";
+divSquare2.style.width = "150px";
+divSquare2.style.height = "150px";
+divSquare2.style.backgroundColor = "#002e11";
+document.body.append(divSquare2);
 
-const button = document.createElement("button");
-button.textContent = "Кнопка";
-button.style.padding = '5px 10px';
-button.style.backgroundColor = 'rgb(38, 38, 38)';
-button.style.color = '#fff';
-button.style.border = 'none';
-button.style.borderRadius = '5px';
-button.style.cursor = 'pointer';
-document.body.append(button);
+const button3 = document.createElement("button3");
+button3.textContent = "Button3";
+button3.style.padding = '5px 10px';
+button3.style.backgroundColor = 'rgb(38, 38, 38)';
+button3.style.color = '#fff';
+button3.style.border = 'none';
+button3.style.borderRadius = '5px';
+button3.style.cursor = 'pointer';
+document.body.append(button3);
 
-const square = document.querySelector("#mySquare");
+const square150 = document.querySelector("#mySquare2");
 
-button.addEventListener("click", function () {
-    const currentWidth = square.offsetWidth;// получение тикущей ширины 
-    const currentHeight = square.offsetHeight;// полу
+button3.addEventListener("click", function () {
+    const currentWidth = square150.offsetWidth;// получение тикущей ширины 
+    const currentHeight = square150.offsetHeight;// полyчение текущей высоты
     const newWidth = currentWidth + 20;
     const newHeight = currentHeight + 20;
-    square.style.width = `${newWidth}px`;
-    square.style.height = `${newHeight}px`;
+    square150.style.width = `${newWidth}px`;
+    square150.style.height = `${newHeight}px`;
 });
 
 // The fourth task: Создать кнопку и div с классом root. При клике на кнопку в div создается параграф синего цвета. Текст параграфа произвольный.
 
-const divSquare = document.createElement("div");
-divSquare.classList.add("root");
-document.body.append(divSquare);
+const newDiv = document.createElement("div");
+newDiv.classList.add("root");
+document.body.append(newDiv);
 
-const button = document.createElement("button");
-button.textContent = "Кнопка";
-button.style.padding = '5px 10px';
-button.style.backgroundColor = 'rgb(38, 38, 38)';
-button.style.color = '#fff';
-button.style.border = 'none';
-button.style.borderRadius = '5px';
-button.style.cursor = 'pointer';
-document.body.append(button);
+const button4 = document.createElement("button4");
+button4.textContent = "Button4";
+button4.style.padding = '5px 10px';
+button4.style.backgroundColor = 'rgb(38, 38, 38)';
+button4.style.color = '#fff';
+button4.style.border = 'none';
+button4.style.borderRadius = '5px';
+button4.style.cursor = 'pointer';
+document.body.append(button4);
 
-button.addEventListener("click", function () {
+button4.addEventListener("click", function () {
     const newParagraph = document.createElement("p");
     newParagraph.style.color = "blue";
     newParagraph.textContent = "Some text";
@@ -106,21 +109,21 @@ button.addEventListener("click", function () {
 
 // The fifth task: Создать кнопку и div с классом root. При клике на кнопку в div создаются по очереди параграфы синего и зеленого цветов. Первый цвет синий.
 
-const div = document.createElement("div");
-div.classList.add("root");
-document.body.append(div);
+const divTaskFirth = document.createElement("div");
+divTaskFirth.classList.add("root");
+document.body.append(divTaskFirth);
 
-const button = document.createElement("button");
-button.textContent = "Кнопка";
-button.style.padding = '5px 10px';
-button.style.backgroundColor = 'rgb(38, 38, 38)';
-button.style.color = '#fff';
-button.style.border = 'none';
-button.style.borderRadius = '5px';
-button.style.cursor = 'pointer';
-document.body.append(button);
+const button5 = document.createElement("button");
+button5.textContent = "Button5";
+button5.style.padding = '5px 10px';
+button5.style.backgroundColor = 'rgb(38, 38, 38)';
+button5.style.color = '#fff';
+button5.style.border = 'none';
+button5.style.borderRadius = '5px';
+button5.style.cursor = 'pointer';
+document.body.append(button5);
 
-button.addEventListener('click', function () {
+button5.addEventListener('click', function () {
     const blueParagraph = document.createElement('p');
     blueParagraph.style.color = 'blue';
     blueParagraph.innerText = 'The blue paragraph';
@@ -129,6 +132,6 @@ button.addEventListener('click', function () {
     greenParagraph.style.color = 'green';
     greenParagraph.innerText = 'The green paragraph';
 
-    div.append(blueParagraph);
-    div.append(greenParagraph);
+    divTaskFirth.append(blueParagraph);
+    divTaskFirth.append(greenParagraph);
 });
